57436a1b8abe6d4ae8a516718054765c2262d86d
kochamatlaba <161880770+kochamatlaba@users.noreply.github.com>
2024-03-13 23:11:19 +0100

    Update lab2_ania.py

--- lab2_ania.py
+++ lab2_ania.py
@@ -1,10 +1,7 @@
 import random as rnd
 import time as tm
 import matplotlib.pyplot as plt
-#import tabulate as tbl
-import numpy as np
-import copy as copy  # copy to wbudowana metoda tablic nie trzeba jej importowac
-
+import tabulate as tbl
 
 def generowanie_wektora(min, max):
     sizes=[50,100,200,1000,2000,5000]
@@ -24,9 +21,24 @@ def bubble_sort(wektor1):
                 wektor1[j], wektor1[j+1] = wektor1[j+1], wektor1[j]
     if not zamiana: #jeżeli nie nastąpiła żadna zamiana to lista została posortowana
         return
-    end = tm.perf_counter_ns()
+    end=tm.perf_counter_ns()
     czas_bubble_sort=end-start
     return czas_bubble_sort
+
+def selection_sort(wektor2):
+    start = tm.perf_counter_ns()
+    l=len(wektor2)
+    for i in range (l):
+        min_el=wektor2[i]  #przypisanie że pierwszy indeks ma wartość element o najmniejszej wartość
+        for j in range (i+1, l):
+            if wektor2[j]<wektor2[i]: #sprawdzamy czy kolejny element jest mniejszy od naszego min_el
+                min_el=j
+                wektor2[i], wektor2[min_el]=wektor2[min_el], wektor2[i] # jeżeli tak, nastepuje zamiana miejsc
+
+    end=tm.perf_counter_ns()
+    czas_sel_sort=end-start
+    return czas_sel_sort
+
 def insertion_sort(wektor3):
     start=tm.perf_counter_ns()
     l=len(wektor3)
@@ -41,21 +53,6 @@ def insertion_sort(wektor3):
     czas_ins_sort=end-start
     return czas_ins_sort
 
-
-def selection_sort(arr):
-    start = tm.perf_counter_ns()
-    for j in range(0, len(arr)):
-        top = 0
-        for i in range(0, len(arr) - j):
-            if arr[i] > top:
-                top = arr[i]
-        arr[arr.index(top)], arr[-1 - j] = arr[-1 - j], arr[arr.index(top)]
-    end = tm.perf_counter_ns()
-    return end - start
-'''print(generowanie_wektora(10,10000))# usunac
-N = 5
-min=0 # dobra praktyka nie uzywac nazw funkcji wbudowanych jako nazwy zmiennych, polecam MS
-max=10''' # mozna usunac
 wektory=generowanie_wektora(10,1000)
 w1=wektory.copy()
 w2=wektory.copy()
@@ -66,50 +63,52 @@ print(w3)
 czas_bubble=[]
 czas_ins=[]
 czas_sel=[]
+
 for i in range(0,6):
     czas_bubble.append(bubble_sort(w1[i]))
     czas_ins.append(insertion_sort(w2[i]))
     czas_sel.append(selection_sort(w3[i]))
 '''wektor_podany=generowanie_wektora(min, max)
 print(wektor_podany)'''
+
 print(czas_ins)
 print(czas_sel)
 print(czas_bubble)
+
 N=int(input("Podaj długość wektora: "))
 min=int(input("Podaj minimalną wartość wektora: "))
 max=int(input("Podaj maksymalną wartość wektora: "))
+
 wektor=generowanie_wektora(min,max)
 wektor1=wektor.copy()
 wektor2=wektor.copy()
 wektor3=wektor.copy()
 print(wektor)
 print(wektor1)
-# najpierw funkcje potem zmienne
 
-#czas_bubble_sort=bubble_sort(wektor1)# przypisana zmienna do wartości funkcji
+czas_bubble_sort=bubble_sort(wektor1)
 
 print("Wektor posortowany przy użyciu sortowania bąbelkowego: ", wektor1)
-print("Czas sortowania:", "nanosekund") # zamiast tm.perf_couter_ns dalem wartosc czasu bubble sorta itd w kazdym innym
+print("Czas sortowania:",czas_bubble_sort "nanosekund")
 
-
-#czas_sel_sort=selection_sort(wektor2) # przypisana zmienna do wartości funkcji
+czas_sel_sort=selection_sort(wektor2)
 print("Wektor posortowany przy użyciu sortowania poprzez wybór: ", wektor2)
-print("Czas sortowania:", "nanosekund")
+print("Czas sortowania:",czas_sel_sort, "nanosekund")
 
-#czas_ins_sort=insertion_sort(wektor3)
+czas_ins_sort=insertion_sort(wektor3)
 print("Wektor posortowany przy użyciu sortowania przez wstawianie: ", wektor3)
-print("Czas sortowania:", "nanosekund")
+print("Czas sortowania:", czas_ins_sort, "nanosekund")
 
-# zmieniłem wykres tak zeby cokolwiek pokazywał, przy jednej próbie należałoby użyć wykresu puktowego, bo inaczej nic sie nie wyswietla
 def rysowanie_wykresu():
     plt.plot([50, 100, 200, 1000, 2000, 5000], czas_ins, label="Insertion sort")
     plt.plot([50,100,200,1000,2000,5000],czas_bubble, label="Bubble sort")
     plt.plot([50,100,200,1000,2000,5000],czas_sel, label="Selection sort")
-    plt.xlabel('Rozmiar wektora')
-    plt.ylabel('Runtime (ns)')
-    plt.title('Runtime Comparison of Sorting Algorithms')
+    plt.xlabel("Rozmiar wektora")
+    plt.ylabel("Czas sortowania [nanosekundy]")
+    plt.title("Porównanie czasu sortowania algorytmów")
     plt.legend()
     plt.grid(True)
     plt.show()
-rysowanie_wykresu()# to nic nie robi
-# wykres sie nie rysuje bo N nie jest tabicą rozmiarów prób
+rysowanie_wykresu()
+#bomba ale nie wiem co zmieniłeś w tym swoim kodzie i co porpawić
+#zajebałam sie już z tymi wektorami i nie wiem co to sortuje?!

38c9226e24d0a8edb45ce550d94236b71f411131
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-03-14 17:27:03 +0100

    Lab 3, polecam użyć klasy do przechowywania statystyk

--- Lab3_Mikolaj.py
+++ Lab3_Mikolaj.py
@@ -2,24 +2,25 @@ from random import randint
 import time as tm
 import matplotlib.pyplot as plt
 
+# klasa results służąca do przechowywania danych sortowań i licząca ich parametry
 class Results:
-    def __init__(self):
-        self.time50=[]
-        self.time100=[]
-        self.time200=[]
-        self.time500=[]
-        self.time1000=[]
-        self.time2000=[]
-
-class R1(Results):
+    # ważne! tak się deklaruje klasy w python, bardzo przydatne, warto umieć! (napisanie jednej klasy zaoszczędziło mi pisania 100 linijek kodu dla tego zadania)
     def __init__(self,time_arr):
         super().__init__()
         self.time_arr=time_arr
+        self.time50 = []
+        self.time100 = []
+        self.time200 = []
+        self.time500 = []
+        self.time1000 = []
+        self.time2000 = []
         self.mins=[]
         self.maxs=[]
         self.means=[]
         self.separate_arrays()
         self.get_stats()
+
+    # metoda separate arrays służąca do wydobycia danych o czasie sortowania tablicy o określonym rozmiarze, ważne jest,żeby przy inicjalizacji klasy podac wektor wektorów (funkcja generate vectors)
     def separate_arrays(self):
         for i in range(0,len(self.time_arr)):
             self.time50.append(self.time_arr[i][0])
@@ -28,13 +29,18 @@ class R1(Results):
             self.time500.append(self.time_arr[i][3])
             self.time1000.append(self.time_arr[i][4])
             self.time2000.append(self.time_arr[i][5])
-        print(self.time100)
+
+    # metoda licząca statystyki w każdej z wydzielonych wcześniej tabel
     def get_stats(self):
         args=[self.time50,self.time100,self.time200,self.time500,self.time1000,self.time2000]
+
+        # pętla dla każdej z tabel liczy jej statystyki i dodaje do tabeli statycstycznych
         for arr in args:
             self.mins.append(min(arr))
             self.maxs.append(max(arr))
             self.means.append(sum(arr)/len(arr))
+
+# w celu sprawdzenia czy sortowania działają należy zrobić coś takiego print(bubble_sort(jakas_tablica)[1]), gdyż funkcje sortujące zwracają wynik w takiej postaci (czas,posortowana_tablica), wtedy posortowana tablica znajduje się pod indeksem 1, taka typ danych nazywa się tuple
 def bubble_sort(arr):
     start = tm.perf_counter_ns()
     a = 1
@@ -76,13 +82,7 @@ def selection_sort(arr):
     end = tm.perf_counter_ns()
     return end - start, arr
 
-
-
-bubble_time = []
-insert_time = []
-selection_time = []
-sizes = [50, 100, 200, 500, 1000, 2000]
-
+# funkcja tworząca wektor zawierający wektory 50,100,200,500,1000 i 2000 elementowe, osobne dla każdego z sortowań, żeby uniknąć sortowania juz posortowanej tabeli
 def generate_vector():
     bubble_arrays = []
     insert_arrays = []
@@ -94,34 +94,62 @@ def generate_vector():
         selection_arrays.append(temporary.copy())
     return bubble_arrays,insert_arrays,selection_arrays
 
-'''bubble_time = [bubble_sort(bubble_arrays[i])[0] for i in range(0, 6)]
-insert_time = [insertion_sort(insert_arrays[i])[0] for i in range(0, 6)]
-selection_time = [selection_sort(selection_arrays[i])[0] for i in range(0, 6)]'''
 
+bubble_time = []
+insert_time = []
+selection_time = []
+sizes = [50, 100, 200, 500, 1000, 2000]
 
-for i in range(0,10):
-    bubble_arrays,insertion_arrays,selection_arrays=generate_vector()
-    bubble_time.append([bubble_sort(bubble_arrays[i])[0] for i in range(0, 6)])
+# 100 krotne powtórzenie sortowań (Metoda Monte Carlo), w instrukcji jest żeby ilość iteracji podawać z klawiatury (nie chce mi sie)
+for i in range(0,100):
+    bubble_arrays,insertion_arrays,selection_arrays=generate_vector() # tworzenie nowych wektorów próbek dla każdego sortowania
+    bubble_time.append([bubble_sort(bubble_arrays[i])[0] for i in range(0, 6)]) # w każdej iteracji do tabeli czasów dodawane są kolejne czasy sortowania dla każdego rozmiaru tabeli wejściowej
     insert_time.append([insertion_sort(insertion_arrays[i])[0] for i in range(0, 6)])
     selection_time.append([selection_sort(selection_arrays[i])[0] for i in range(0, 6)])
-bubble=R1(bubble_time)
-insertion=R1(insert_time)
-selection=R1(selection_time)
 
-fig,axs=plt.subplots(2)
-axs[0].plot(sizes,bubble.means,label="Bubble sort means")
-axs[0].plot(sizes,insertion.means,label="Insertion sort mean")
-axs[0].plot(sizes,selection.means,label="Selection sort mean")
+# inicjalizacja klasy Results dla wszystkich sortowań, ważne, żeby podać czasy! Klasa sama wyznaczy sobie już wszystko, chillerka :)
+bubble=Results(bubble_time)
+insertion=Results(insert_time)
+selection=Results(selection_time)
 
-axs[0].legend()
-axs[0].grid(True)
-plt.show()
-'''plt.plot(sizes, bubble_time, label="Bubble sort")
-plt.plot(sizes, insert_time, label='Insertion sort')
-plt.plot(sizes, selection_time, label='Selection sort')
-plt.xlabel('Vector size')
-plt.ylabel('Sorting time [ns]')
-plt.title('Runtime Comparison of Sorting Algorithms')
+plt.figure(1)
+plt.plot(sizes,bubble.means,label="Bubble sort means")
+plt.plot(sizes,insertion.means,label="Insertion sort mean")
+plt.plot(sizes,selection.means,label="Selection sort mean")
+plt.title("Mean sorting time for sorting algorithms")
+plt.xlabel("Array size")
+plt.ylabel("Sorting time")
 plt.legend()
 plt.grid(True)
-plt.show()'''
+
+plt.figure(2)
+plt.scatter(sizes,bubble.mins,label="mins",s=10)
+plt.scatter(sizes,bubble.maxs,label="maxs",s=10)
+plt.scatter(sizes,bubble.means,label="means",s=10)
+plt.title("Bubble sort statistics for different array sizes")
+plt.xlabel("Array size")
+plt.ylabel("Sorting time")
+plt.legend()
+plt.grid(True)
+
+plt.figure(3)
+plt.scatter(sizes,insertion.mins,label="mins",s=10)
+plt.scatter(sizes,insertion.maxs,label="maxs",s=10)
+plt.scatter(sizes,insertion.means,label="means",s=10)
+plt.title("Insertion sort statistics for different array sizes")
+plt.xlabel("Array size")
+plt.ylabel("Sorting time")
+plt.legend()
+plt.grid(True)
+
+plt.figure(4)
+plt.scatter(sizes,selection.mins,label="mins",s=10)
+plt.scatter(sizes,selection.maxs,label="maxs",s=10)
+plt.scatter(sizes,selection.means,label="means",s=10)
+plt.title("Selection sort statistics for different array sizes")
+plt.xlabel("Array size")
+plt.ylabel("Sorting time")
+plt.legend()
+plt.grid(True)
+
+plt.show()

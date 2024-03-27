4979ff6bb81006678fff5227230e259a79cb0789
kochamatlaba <aniatadel17@gmail.com>
2024-03-27 21:22:07 +0100

    es

--- Ania/lab4.py
+++ Ania/lab4.py
@@ -1,71 +1,225 @@
-import random as rnd
+from random import randint
 import time as tm
 import matplotlib.pyplot as plt
+import tabulate as tbl
+import pandas as pd
+from tabulate import tabulate
 
+class Wyniki:
+    def __init__(self, czas_wektory):
+        super().__init__()
+        self.czas_wektory=czas_wektory
+        self.czas_50 = []
+        self.czas_100 = []
+        self.czas_200 = []
+        self.czas_500 = []
+        self.czas_1000 = []
+        self.czas_2000 = []
+        self.mins=[]
+        self.maxs=[]
+        self.means=[]
+        self.oddzielne_wektory()
+        self.stat_sort()
 
-def merge_sort(wektor):
-    if len(wektor)>1:
-        srodek=(len(wektor) // 2) #dzielenie wektoru na 2 równe części, lewą i prawą
-        lewa = wektor[:srodek]
-        prawa = wektor[srodek:]
-        merge_sort(lewa)
-        merge_sort(prawa)
+    # metoda do wydobycia danych o czasie sortowania wektora o określonym rozmiarze
+    def oddzielne_wektory(self):
+        for i in range(0,len(self.czas_wektory)):
+            self.czas_50.append(self.czas_wektory[i][0])
+            self.czas_100.append(self.czas_wektory[i][1])
+            self.czas_200.append(self.czas_wektory[i][2])
+            self.czas_500.append(self.czas_wektory[i][3])
+            self.czas_1000.append(self.czas_wektory[i][4])
+            self.czas_2000.append(self.czas_wektory[i][5])
 
-        i=j=k=0 # początkowe indeksy podtablic
+    # metoda licząca statystyki w każdej z wydzielonych wcześniej tabel
+    def stat_sort(self):
+        args=[self.czas_50,self.czas_100,self.czas_200,self.czas_500,self.czas_1000,self.czas_2000]
 
-        while i < len(lewa) and j < len(prawa):
-            if lewa[i] < prawa[j]: # jezeli element na pozycji i (lewa część) jest mniejszy od el na pozycji j (prawa część)
-                wektor[k] = lewa[i] # umieszczamy go w wektorze k na pozycji i
-                i += 1 # zwiększamy indeks tablicy
-            else:
-                wektor[k] = prawa[j] # jeżeli element na pozycji j jest mniejszy, to umieszczamy go w wektorze k na pozycji j
+        # pętla dla każdej z tabel liczy jej statystyki i dodaje do tabeli statycstycznych
+        for wektory in args:
+            self.mins.append(min(wektory))
+            self.maxs.append(max(wektory))
+            self.means.append(sum(wektory)/len(wektory))
+class Sorter:
+    def __init__(self):
+        self.merge_sorted=[]
+        self.counting_sorted=[]
+        self.quicksorted=[]
+
+    def merge_sort(self, wektor):
+        if len(wektor)>1:
+            srodek=(len(wektor) // 2) #dzielenie wektoru na 2 równe części, lewą i prawą
+            lewa = wektor[:srodek]
+            prawa = wektor[srodek:]
+            self.merge_sort(lewa)
+            self.merge_sort(prawa)
+
+            i=j=k=0 # początkowe indeksy podtablic
+
+            while i < len(lewa) and j < len(prawa):
+                if lewa[i] < prawa[j]: # jezeli element na pozycji i (lewa część) jest mniejszy od el na pozycji j (prawa część)
+                    wektor[k] = lewa[i] # umieszczamy go w wektorze k na pozycji i
+                    i += 1 # zwiększamy indeks tablicy
+                else:
+                    wektor[k] = prawa[j]
+                    j += 1
+                k += 1 # zwiększamy indeks tablicy docelowej
+
+            # scalanie części lewej i prawej
+            while i < len(lewa):
+                wektor[k] = lewa[i]
+                i += 1
+                k += 1
+
+            while j < len(prawa):
+                wektor[k] = prawa[j]
                 j += 1
-            k += 1 # zwiększamy indeks tablicy docelowej
+                k += 1
+        return wektor
 
-        # scalanie części lewej i prawej
-        while i < len(lewa):
-            wektor[k] = lewa[i]
-            i += 1
-            k += 1
+    def counting_sort(self, wektor):
+        maks=max(wektor)
+        mini=min(wektor)
+        posort=[] #posorotwane
+        k=[0]*(maks+1) # lista zer, jej długość jest ustalana na podstawie maksymalnej wartości w wektorze zwiększonej o 1
+        for i in range(len(wektor)): #przechodzenie przez całą długość wektora
+            k[wektor[i]]= k[wektor[i]] + 1 # dla każdego występowania danego elementu wektora i zwiększamy o jednen liczbę zer
+        for j in range(len(k)): # przechodzimy przez listę zer
+            if k[j]!=0: # jeżeli element j jest różny od 0, to dodajemy go do posortowanej części
+                posort.append(j)
+        return posort
 
-        while j < len(prawa):
-            wektor[k] = prawa[j]
-            j += 1
-            k += 1
-    return wektor
+    def quick_sort(self, wektor): #zaczęte
+        if len(wektor)>1:
+            pivot = wektor[-2]
+            l = 0
+            r = len(wektor) - 1
+            while l < r:
+                if wektor[l] > pivot and wektor[r] < pivot:
+                    wektor[l], wektor[r] = wektor[r], wektor[l]
+                elif wektor[l] == pivot and wektor[r] < pivot:
+                    wektor[l], wektor[r] = wektor[r], wektor[l]
+                    l += 1
+                elif wektor[r] == pivot and wektor[l] > pivot:
+                    wektor[l], wektor[r] = wektor[r], wektor[l]
+                    r -= 1
+                elif wektor[l] >= pivot:
+                    r -= 1
+                elif wektor[r] <= pivot:
+                    l += 1
+                else:
+                    l += 1
+                    r -= 1
+            c = wektor.count(pivot)
+            left = wektor[:wektor.index(pivot) + c]
+            right = wektor[wektor.index(pivot) + c:]
+            if not left or not right:
+                wektor = left + right
+                return wektor
+            a = self.quick_sort(left)
+            b = self.quick_sort(right)
+            wektor = a + b
+        return wektor
 
-def counting_sort(wektor):
-    maks=max(wektor)
-    mini=min(wektor)
-    posort=[] #posorotwane
-    k=[0]*(maks+1) # lista zer, jej długość jest ustalana na podstawie maksymalnej wartości w wektorze zwiększonej o 1
-    for i in range(len(wektor)): #przechodzenie przez całą długość wektora
-        k[wektor[i]]= k[wektor[i]] + 1 # dla każdego elementu wektora i zwiększamy o jednen liczbę zer
-    for j in range(len(k)): # przechodzimy przez listę zer
-        if k[j]!=0: # jeżeli element j jest różny od 0, to dodajemy go do posortowanej części
-            posort.append(j)
-    return posort
+def generowanie_wektorow():
+    merge_wektory = []
+    counting_wektory = []
+    quick_wektory = []
+    for i in range(0, len(rozmiary)):
+        temporary = [randint(0, 5000) for _ in range(0, rozmiary[i])]
+        merge_wektory.append(temporary.copy())
+        counting_wektory.append(temporary.copy())
+        quick_wektory.append(temporary.copy())
+    return merge_wektory,counting_wektory,quick_wektory
 
-def quick_sort(wektor): #zaczęte
-    if len(wektor)>1:
-        a = wektor[-2]
-        l = 0
-        p = len(wektor) - 1
-        while l<p:
-            print(a)
-            print(wektor)
-            print(l,p)
-            if wektor[l]>a and wektor[p]<a:
-                wektor[l],wektor[p] = wektor[p],wektor[l]
-                p-=1
-                l+=1
+merge_czas = []
+counting_czas = []
+quick_czas = []
+rozmiary = [50, 100, 200, 500, 1000, 2000]
+sorter=Sorter()
 
-wektor=[5,4,2,100,9,250]
-posortowany_wektor=counting_sort(wektor)
-print(posortowany_wektor)
+# 100 krotne powtórzenie sortowań (Metoda Monte Carlo)
+for i in range(0,100):
+    merge_wektory, counting_wektory, quick_wektory = generowanie_wektorow()
+    m_temp = []
+    c_temp = []
+    q_temp = []
+    for j in range(6):
+        m_start = tm.perf_counter_ns()
+        sorter.merge_sort(merge_wektory[j])
+        m_end = tm.perf_counter_ns()
+        m_temp.append(m_end - m_start)
+
+        c_start = tm.perf_counter_ns()
+        sorter.counting_sort(counting_wektory[j])
+        c_end = tm.perf_counter_ns()
+        c_temp.append(c_end - c_start)
+
+        q_start = tm.perf_counter_ns()
+        sorter.quick_sort(quick_wektory[j])
+        q_end = tm.perf_counter_ns()
+        q_temp.append(q_end - q_start)
+
+    merge_czas.append(m_temp)
+    counting_czas.append(c_temp)
+    quick_czas.append(q_temp)
 
 
-# rozmiary = [50, 100, 200, 500, 1000, 2000]
-# for i in range(0, len(rozmiary)):
-#     temporary = [rnd.randint(0, 5000) for _ in range(0, rozmiary[i])]
-#     merge_sort.append(temporary.copy())
+# inicjalizacja klasy Results dla wszystkich sortowań, ważne, żeby podać czasy! Klasa sama wyznaczy sobie już wszystko, chillerka :)
+merge=Wyniki(merge_czas)
+counting=Wyniki(counting_czas)
+quick=Wyniki(quick_czas)
+
+plt.figure(1)
+plt.plot(rozmiary, merge.means, label="Merge sort średni czas")
+plt.plot(rozmiary, counting.means, label="Counting sort średni czas")
+plt.plot(rozmiary, quick.means, label="Quick sort średni czas")
+plt.title("Średni czas algorytmów sortujących")
+plt.xlabel("Rozmiar wektora")
+plt.ylabel("Czas sortowania")
+plt.legend()
+plt.grid(True)
+
+plt.figure(2)
+plt.scatter(rozmiary, merge.mins, label="min", s=10)
+plt.scatter(rozmiary, merge.maxs, label="max", s=10)
+plt.scatter(rozmiary, merge.means, label="śr", s=10)
+plt.title("Statystyki merge sort'a dla różnych rozmiarów wektora")
+plt.xlabel("Rozmiar wektora")
+plt.ylabel("Czas sortowania")
+plt.legend()
+plt.grid(True)
+
+plt.figure(3)
+plt.scatter(rozmiary, counting.mins, label="min", s=10)
+plt.scatter(rozmiary, counting.maxs, label="max", s=10)
+plt.scatter(rozmiary, counting.means, label="śr", s=10)
+plt.title("Statystyki counting sort'a dla różnych rozmiarów wektora")
+plt.xlabel("Rozmiar wektora")
+plt.ylabel("Czas sortowania")
+plt.legend()
+plt.grid(True)
+
+plt.figure(4)
+plt.scatter(rozmiary, quick.mins, label="min", s=10)
+plt.scatter(rozmiary, quick.maxs, label="max", s=10)
+plt.scatter(rozmiary, quick.means, label="śr", s=10)
+plt.title("Statystyki quick sort'a dla różnych rozmiarów wektora")
+plt.xlabel("Rozmiar wektora")
+plt.ylabel("Czas sortowania")
+plt.legend()
+plt.grid(True)
+
+plt.show()
+
+tabela={
+    "merge sort": merge_czas,
+    "counting sort": counting_czas,
+    "quick sort": quick_czas
+}
+df = pd.DataFrame(tabela, index = rozmiary)
+print(tbl.tabulate({"Rozmiary": rozmiary,"Merge sort [ns]": merge_czas,"Counting sort [ns]": counting_czas,"Quick sort [ns]": quick_czas}, headers="keys", tablefmt="mixed_grid"))
+
+
+
+

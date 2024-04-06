ae364446ff2f1a8da8e3bcecf2294fd4b73048a6
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-04-06 20:11:29 +0200

    obraz grafu

--- Mikolaj/Lab5_Mikolaj_heap.py
+++ Mikolaj/Lab5_Mikolaj_heap.py
@@ -1,4 +1,6 @@
 import random
+import networkx as nx
+import matplotlib.pyplot as plt
 class TreeNode:
     """
     Individual node of a Tree class.
@@ -66,6 +68,7 @@ class MaxHeap:
     Max Heap data structure.
     """
     def __init__(self,arr):
+        self.graph=[]
         self.arr=arr
         self.root=TreeNode(max(self.arr))
         self.arr.remove(max(self.arr))
@@ -86,12 +89,14 @@ class MaxHeap:
             if not current.left:
                 print(f"{current.val}  added {val} left")
                 current.left = TreeNode(val)
+                self.graph.append((f'{current.val}',f'{current.left.val}'))
                 break
             else:
                 q.append(current.left)
             if not current.right:
                 print(f"{current.val}  added {val} right")
                 current.right = TreeNode(val)
+                self.graph.append((f'{current.val}',f'{current.right.val}'))
                 break
             else:
                 q.append(current.right)
@@ -106,6 +111,7 @@ class MaxHeap:
             m=max(temp)
             self.add_node(self.root,m)
             temp.remove(m)
+        return self.graph
 def heapsort(arr:list)->list: # heapsort bazujący na tworzeniu klasy, mega nieoptymalny nie używam go w porównaniu
     """
     Heap sort the array. Time complexity O(n*log(n))
@@ -128,17 +134,91 @@ def heapsort(arr:list)->list: # heapsort bazujący na tworzeniu klasy, mega nieo
 
 def generate_vector():
     t=[]
-    for i in range(0,10):
-        t.append(random.randint(1,10))
+    used=[]
+    while len(t)<10:
+        a=random.randint(1,50)
+        if a not in used:
+            t.append(a)
+            used.append(a)
     return t
 
-'''
+
 s=Tree([1,2,3,4,5])
 s.make_tree()
 s.print_tree(s.root)
-
-h=MaxHeap([1,2,3,4,5])
-h.make_heap()'''
 vec=generate_vector()
+h=MaxHeap(vec)
+g=h.make_heap()
+print(g)
+G=nx.Graph()
+G.add_edges_from(g)
+'''vec=generate_vector()
 print(vec)
-print(heapsort(vec))
+print(heapsort(vec))'''
+
+# grafowanie z neta wzięte
+def hierarchy_pos(G, root=None, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5):
+    '''
+    From Joel's answer at https://stackoverflow.com/a/29597209/2966723.
+    Licensed under Creative Commons Attribution-Share Alike
+
+    If the graph is a tree this will return the positions to plot this in a
+    hierarchical layout.
+
+    G: the graph (must be a tree)
+
+    root: the root node of current branch
+    - if the tree is directed and this is not given,
+      the root will be found and used
+    - if the tree is directed and this is given, then
+      the positions will be just for the descendants of this node.
+    - if the tree is undirected and not given,
+      then a random choice will be used.
+
+    width: horizontal space allocated for this branch - avoids overlap with other branches
+
+    vert_gap: gap between levels of hierarchy
+
+    vert_loc: vertical location of root
+
+    xcenter: horizontal location of root
+    '''
+    if not nx.is_tree(G):
+        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
+
+    if root is None:
+        if isinstance(G, nx.DiGraph):
+            root = next(iter(nx.topological_sort(G)))  # allows back compatibility with nx version 1.11
+        else:
+            root = random.choice(list(G.nodes))
+
+    def _hierarchy_pos(G, root, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5, pos=None, parent=None):
+        '''
+        see hierarchy_pos docstring for most arguments
+
+        pos: a dict saying where all nodes go if they have been assigned
+        parent: parent of this branch. - only affects it if non-directed
+
+        '''
+
+        if pos is None:
+            pos = {root: (xcenter, vert_loc)}
+        else:
+            pos[root] = (xcenter, vert_loc)
+        children = list(G.neighbors(root))
+        if not isinstance(G, nx.DiGraph) and parent is not None:
+            children.remove(parent)
+        if len(children) != 0:
+            dx = width / len(children)
+            nextx = xcenter - width / 2 - dx / 2
+            for child in children:
+                nextx += dx
+                pos = _hierarchy_pos(G, child, width=dx, vert_gap=vert_gap,
+                                     vert_loc=vert_loc - vert_gap, xcenter=nextx,
+                                     pos=pos, parent=root)
+        return pos
+
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
+pos = hierarchy_pos(G,f'{h.root.val}') # tutaj dodać root
+nx.draw(G, pos=pos, with_labels=True)
+plt.savefig('hierarchy.png')

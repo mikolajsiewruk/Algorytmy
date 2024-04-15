d743a350aa043b9e1ffd12db5ac5b17d071953be
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-04-15 21:54:30 +0200

    lab 6 v1

--- Mikolaj/Lab6_Mikolaj.py
+++ Mikolaj/Lab6_Mikolaj.py
@@ -4,11 +4,23 @@ class TreeNode:
         self.value = value
         self.left = None
         self. right = None
+        self.balance = 0
 
 class BST:
     def __init__(self, arr: list):
         self.arr = arr
         self.root = TreeNode(self.arr[0])
+        self.make_tree()
+
+    def make_tree(self):
+        for nums in self.arr[1:]:
+            self.insert(self.root,nums)
+
+    def traverse(self,node):
+        if node is not None:
+            self.traverse(node.left)
+            print(node.value)
+            self.traverse(node.right)
 
     def insert(self,node,value):
         if node.value:
@@ -36,46 +48,114 @@ class BST:
                     if node.right == None:
                         return 1
                     else:
-                        return self.search(node.left, value)
+                        return self.search(node.right, value)
                 else:
                     if node.left == None:
                         return 1
                     else:
                         return self.search(node.left, value)
-    def remove(self,node,value): # change
+    def remove(self,node,value):
         if not node:
             return 1
         else:
-            if node.value == value:
+            if node.value > value:
+                node.left = self.remove(node.left,value)
+                return node
+            elif node.value < value:
+                node.right = self.remove(node.right,value)
+                return node
+            else:
                 if not node.left and not node.right:
-                    node = None
+                    del node
                 elif not node.left:
                     a = node.right.value
-                    node.right = None
+                    del node.right
                     node = TreeNode(a)
+                    return node
 
-                elif not node.left:
+                elif not node.right:
                     a = node.left.value
-                    node.left = None
+                    del node.left
                     node = TreeNode(a)
+                    return node
                 else:
+                    parent = node
+                    successor = node.right
+                    while successor.left is not None:
+                        parent = successor
+                        successor = successor.left
+                        if parent != node:
+                            parent.left = successor.right
+                        else:
+                            parent.right = successor.right
+                        del successor
+                        return node
+    '''def find_parent(self,value):
+        node = self.search(self.root,value)
+        return self.traverse(node)'''
 
+
+class AVL(BST):
+
+    def check_balance(self,node):
+        if node == None:
+            return 0
+        else:
+            return max(self.check_balance(node.left),self.check_balance(node.right)) + 1
+
+    def rotate_right(self,node):
+        R = node.left
+        node.left = R.right
+        R.right = node
+        node = R
+
+    def rotate_left(self,node):
+        L = node.right
+        node.right = L.left
+        L.left = node
+        node = L
+
+    def balance(self,node):
+        balance_factor = self.check_balance(node.left) - self.check_balance(node.right)
+        while balance_factor not in [-1,0,1]:
+            # print(balance_factor)
+            if balance_factor > 1:
+                # print('left lacking')
+                self.rotate_right(node)
             else:
-                if value > node.value:
-                    if node.right == None:
-                        return 1
-                    else:
-                        return self.search(node.left, value)
+                # print('right')
+                self.rotate_left(node)
+            balance_factor = self.check_balance(node.left) - self.check_balance(node.right)
+            # print(node.value)
+    def insert(self,node,value): # fix to check every node for balance
+        if node.value:
+            if value > node.value:
+                if node.right is None:
+                    node.right = TreeNode(value)
                 else:
-                    if node.left == None:
-                        return 1
-                    else:
-                        return self.search(node.left, value)
-a=[5,1,8,3,10]
+                    self.insert(node.right,value)
+            else:
+                if node.left is None:
+                    node.left = TreeNode(value)
+                else:
+                    self.insert(node.left,value)
+        balance_factor = self.check_balance(node.left) - self.check_balance(node.right)
+        if balance_factor not in [-1,0,1]:
+            self.balance(node)
+
+
+
+
+'''a=[5,1,8,7,3,10,11,13,14,15,16,17]
 b=BST(a)
 for el in a[1:]:
     b.insert(b.root,el)
 b.insert(b.root,4)
-c=b.search(b.root,1)
-print(c)
-#print(b.root.left.value,b.root.right.value)
+b.remove(b.root,4)
+avl = AVL(a)
+ elem in a[1:]:
+    print(elem)
+    avl.insert(avl.root,elem)
+print(avl.root.value)
+print(avl.check_balance(avl.root.left),avl.check_balance(avl.root.right))
+#print(b.root.left.value,b.root.right.value)'''
---
+++ Mikolaj/lab6_mikolaj_use.py
@@ -0,0 +1,72 @@
+from Lab6_Mikolaj import BST, AVL
+from Lab3_Mikolaj import Results
+import numpy as np
+import random
+import time as tm
+import matplotlib.pyplot as plt
+
+
+def generate():
+    sizes = [50, 100, 200, 500, 1000, 2000]  # not the best design
+    bst_arrays = []
+    avl_arrays = []
+    arr = []
+    for j in range(len(sizes)):
+        for i in range(1, sizes[j]+1):
+            arr.append(i)
+        np.random.shuffle(arr)
+        temp = arr.copy()
+        bst_arrays.append(temp)
+        avl_arrays.append(temp)
+    return bst_arrays, avl_arrays
+
+
+sizes = [50, 100, 200, 500, 1000, 2000]
+time_bst = []
+time_avl = []
+
+for k in range(100):
+    arr = generate()
+    t_bst = []
+    t_avl = []
+    for i in range(len(sizes)):
+        bst = BST(arr[0][i])
+        avl = AVL(arr[1][i])
+        print(avl.check_balance(avl.root.right),avl.check_balance(avl.root.left))
+
+        values = []
+        time_bst_temp = 0
+        time_avl_temp = 0
+
+        for j in range(sizes[i]): # mega ważne!!!! jeśli tutaj da się 100 tak jak jest w instrukcji, wykres avl wygląda jakby nie miał sensu, jednakże ma on właśnie wielki sens tylko trzeba to umieć uzasadnić
+            values.append(random.randint(1, sizes[i]))
+
+        for val in values:
+            t1 = tm.perf_counter_ns()
+            bst.search(bst.root,val)
+            t2 = tm.perf_counter_ns()
+            time_bst_temp += t2-t1
+
+            t3 = tm.perf_counter_ns()
+            avl.search(avl.root,val)
+            t4 = tm.perf_counter_ns()
+            time_avl_temp += t4-t3
+
+        t_bst.append(time_bst_temp)
+        t_avl.append(time_avl_temp)
+    time_bst.append(t_bst)
+    time_avl.append(t_avl)
+
+
+stats_avl = Results(time_avl)
+stats_bst = Results(time_bst)
+print(stats_avl.means,stats_bst.means )
+
+plt.plot(sizes,stats_avl.means, label = "AVL")
+plt.plot(sizes,stats_bst.means, label = "BST")
+plt.title("BST and AVL trees search time comparison")
+plt.xlabel("Array size")
+plt.ylabel("Search time")
+plt.grid()
+plt.legend()
+plt.show()

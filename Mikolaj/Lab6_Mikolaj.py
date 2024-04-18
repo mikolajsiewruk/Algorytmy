5b3e849ab2c7d590c363ff93b9dede7ea90ed8d1
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-04-18 19:35:24 +0200

    lab 6 v5 komentarze

--- Mikolaj/Lab6_Mikolaj.py
+++ Mikolaj/Lab6_Mikolaj.py
@@ -4,19 +4,24 @@ class TreeNode:
         self.value = value
         self.left = None
         self. right = None
-        self.balance = 0
 
 class BST:
     def __init__(self, arr: list):
         self.arr = arr
         self.root = TreeNode(self.arr[0])
-        self.make_tree()
+        self.make_tree()  # z tą linijką kodu drzewa utworzą się automatycznie z wartości podanych w tabeli arr, jednak można dodać inne wartości ręcznie poprzez insert (przykład na dole)
 
     def make_tree(self):
+        """
+        Inserts array values into a Tree structure.
+        """
         for nums in self.arr[1:]:
             self.insert(self.root,nums)
 
     def traverse(self,node):
+        """
+        Prints inorder traverse of a Tree.
+        """
         if node is not None:
             self.traverse(node.left)
             print(node.value)
@@ -24,20 +29,23 @@ class BST:
 
     def insert(self,node,value):
         if node.value:
-            if value > node.value:
+            if value > node.value:  # jeśli jest większe to wstaw z prawej
                 if node.right is None:
                     node.right = TreeNode(value)
                 else:
                     self.insert(node.right,value)
-            else:
-                if node.left is None:
+            else:  # jesli mniejsze to wstaw z lewej
+                if node.left is None:  # jeśli nie ma lewego węzła to wstaw
                     node.left = TreeNode(value)
-                else:
+                else:  # jeśli jest to kontynuuj wstawianie
                     self.insert(node.left,value)
         else:
             node.value = value
 
     def search(self,node,value):
+        """
+        Returns TreeNode object with a given value.
+        """
         if not node:
             return False
         else:
@@ -58,27 +66,27 @@ class BST:
         if not node:
             return 1
         else:
-            if node.value > value:
+            if node.value > value:  # jeśli wartość mniejsza od wartości węzła to usuwaj z lewej
                 node.left = self.remove(node.left,value)
                 return node
             elif node.value < value:
                 node.right = self.remove(node.right,value)
                 return node
-            else:
-                if not node.left and not node.right:
+            else:  # jeśli wartość węzła = value
+                if not node.left and not node.right:  # jeśli nie ma żadnych dzieci można usunąć węzeł i tyle
                     del node
-                elif not node.left:
+                elif not node.left:  # jeśli nie ma lewego dziecka podstawiamy prawe dziecko węzła za węzeł
                     a = node.right.value
                     del node.right
                     node = TreeNode(a)
                     return node
 
-                elif not node.right:
+                elif not node.right:  # jeśli nie ma prawego dziecka podstawiamy lewe dziecko za węzeł
                     a = node.left.value
                     del node.left
                     node = TreeNode(a)
                     return node
-                else:
+                else:  # jeśli istnieją lewe i prawe dziecko robimy to coś
                     parent = node
                     successor = node.right
                     while successor.left is not None:
@@ -113,7 +121,7 @@ class BST:
         if node.left:
             return self.find_max(node.left)
         else:
-            return "TBC"
+            return "TBC"  # to be continued however never will be, weź z neta
 
     def find_next(self,value):
         node = self.search(self.root, value)
@@ -123,16 +131,19 @@ class BST:
             return "TBC"
 
 
-class AVL(BST):
+class AVL(BST):  # klasa avl dziedziczy metody i argumenty przy inizjalizacji od BST, metody zdefiniowane dla BST działają zawsze dla AVL, jedyną różnicą będzie balansowanie drzewa AVL poprzez rotacje przy dodawaniu i usuwaniu węzłów
 
     def check_balance(self,node):
+        """
+        Returns height of left and right subtree of a node.
+        """
         if node == None:
             return 0
         else:
             return max(self.check_balance(node.left),self.check_balance(node.right)) + 1
 
     def rotate_right(self,node):
-        R = node.left
+        R = node.left  # schemat rotacji jak w instrukcji
         node.left = R.right
         R.right = node
         node = R
@@ -144,13 +155,14 @@ class AVL(BST):
         node = L
 
     def balance(self,node):
+        # balance factor to wysokość lewego poddrzewa - wysokość prawego
         balance_factor = self.check_balance(node.left) - self.check_balance(node.right)
         while balance_factor not in [-1,0,1]:
-            if balance_factor > 1:
+            if balance_factor > 1:  # jeśli jest większy niż jeden to znaczy że lewe poddrzewo jest dłuższe, więc należy zrotować węzeł w prawo
                 self.rotate_right(node)
             else:
                 self.rotate_left(node)
-            balance_factor = self.check_balance(node.left) - self.check_balance(node.right)
+            balance_factor = self.check_balance(node.left) - self.check_balance(node.right)  # ponowne obliczenie balance factor gdyby jedna rotacja nie wystarczyła
     def insert(self,node,value):
         if node.value:
             if value > node.value:
@@ -163,6 +175,7 @@ class AVL(BST):
                     node.left = TreeNode(value)
                 else:
                     self.insert(node.left,value)
+        # wstawianie takie samo jak BST z różnicą balansowania
         balance_factor = self.check_balance(node.left) - self.check_balance(node.right)
         if balance_factor not in [-1,0,1]:
             self.balance(node)

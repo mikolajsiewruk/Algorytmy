5f22d5c499e142cbbe846f080299fc3940dbc442
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-05-04 20:58:36 +0200

    Lab 8 dziala,  nieoptymalne

--- Mikolaj/Lab8_Mikolaj.py
+++ Mikolaj/Lab8_Mikolaj.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import messagebox
+import os
 current_player=['O','X']
 root = Tk()
 root.title("Tic tac toe")
@@ -17,29 +19,59 @@ root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
 board = [['' for _ in range(10)] for _ in range(10)]
 buttons = []
-for i in range(3):
+for i in range(10):
     row_buttons = []
-    for j in range(3):
-        button = Button(root,text='', font=("Arial",24),width = 6, height=3, command= lambda row = i, col = j:button_click(row,col))
+    for j in range(10):
+        button = Button(root,text='', font=("Arial",24),width = 3, height=1, command= lambda row = i, col = j:button_click(row,col))
         button.grid(row=i+1,column = j,padx = 5, pady= 3)
         row_buttons.append(button)
     buttons.append(row_buttons)
 
 def button_click(row,col):
-    print(current_player[-1])
     if board[row][col] == '':
         cur = current_player[-1]
         if cur =='X':
+            current_player.clear()
             current_player.append('O')
         else:
+            current_player.clear()
             current_player.append('X')
         board[row][col] = current_player[-1]
         draw_board()
+    if check_win(current_player[-1],row,col):
+        messagebox.showinfo("result",f"{current_player[-1]} won")
+        root.destroy()
+        os.startfile("Lab8_Mikolaj.py")
+    if draw_board():
+        messagebox.showinfo("result", 'draw')
+        root.destroy()
+        os.startfile("Lab8_Mikolaj.py")
 
 def draw_board():
-    for i in range(3):
-        for j in range(3):
+    finished = True
+    for i in range(10):
+        for j in range(10):
             button = buttons[i][j]
             button.config(text=board[i][j])
+            if buttons[i][j]['text'] == '':
+                finished = False
+    return finished
+def check_win(player,x,y):
+    win = False
+    col=row=diag=rdiag=0
+
+    for i in range(5):
+        if buttons[x][i]["text"] ==player:
+            col+=1
+        if buttons[i][y]['text'] == player:
+            row+=1
+        if buttons[i][i]['text'] == player:
+            diag+=1
+        if buttons[i][5-i+1]['text'] == player:
+            rdiag+=1
+    if col == 5 or row == 5 or diag == 5 or rdiag==5:
+        win = True
+    print(col,row,diag,rdiag)
+    return win
 
 root.mainloop()
